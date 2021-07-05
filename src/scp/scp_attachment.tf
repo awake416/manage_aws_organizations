4aab8e01faebbf71879264bddf6148eb121224a6
awake416 <chhikara.vivek@gmail.com>
2021-07-05 22:06:16 +0530

    issue-3 - added scp to control tags access into account and resources

--- src/scp/scp_attachment.tf
+++ src/scp/scp_attachment.tf
@@ -6,7 +6,9 @@ locals {
 
   common_policies = [
     aws_organizations_policy.ScpPolicy2.id,
-    aws_organizations_policy.ScpPolicy3.id]
+    aws_organizations_policy.ScpPolicy3.id,
+    aws_organizations_policy.ScpPolicy17.id
+  ]
   dev_policies = {
     for pair in setproduct(local.common_policies, local.dev_accounts): "${pair[0]}-${pair[1]}" => {
       policy_id = pair[0]
