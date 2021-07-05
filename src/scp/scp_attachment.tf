827abd7d6094519e39865ee659ced4fb5e7255ab
awake416 <chhikara.vivek@gmail.com>
2021-07-05 21:29:05 +0530

    issue-3 - add tag based policies.

--- src/scp/scp_attachment.tf
+++ src/scp/scp_attachment.tf
@@ -1,30 +1,31 @@
 locals {
   dev_accounts = [for name, item in var.accounts : item.id if (length(regexall(".*dev", name)) > 0)]
-  non_prod_accounts = [for name, item in var.accounts : item.id if (length(regexall(".*dev|.*prod", name))<0)]
-  prod_accounts = [for name, item in var.accounts : item.id if (length(regexall(".*dev", name)) > 0)]
+  all_accounts = [for name, item in var.accounts : item.id]
+  prod_accounts = [for name, item in var.accounts : item.id if (length(regexall(".*prod", name)) > 0)]
+  non_prod_accounts = setsubtract(setsubtract(local.all_accounts, local.prod_accounts), local.dev_accounts)
 
   common_policies = [
     aws_organizations_policy.ScpPolicy2.id,
     aws_organizations_policy.ScpPolicy3.id]
   dev_policies = {
-  for pair in setproduct(local.common_policies, local.dev_accounts): "${pair[0]}-${pair[1]}" => {
-    policy_id = pair[0]
-    account_id = pair[1]
-  }
+    for pair in setproduct(local.common_policies, local.dev_accounts): "${pair[0]}-${pair[1]}" => {
+      policy_id = pair[0]
+      account_id = pair[1]
+    }
   }
 
   non_prod_policies = {
-  for pair in setproduct(local.common_policies, local.non_prod_accounts): "${pair[0]}-${pair[1]}" => {
-    policy_id = pair[0]
-    account_id = pair[1]
-  }
+    for pair in setproduct(local.common_policies, local.non_prod_accounts): "${pair[0]}-${pair[1]}" => {
+      policy_id = pair[0]
+      account_id = pair[1]
+    }
   }
 
   prod_policies = {
-  for pair in setproduct(local.common_policies, local.prod_accounts): "${pair[0]}-${pair[1]}" => {
-    policy_id = pair[0]
-    account_id = pair[1]
-  }
+    for pair in setproduct(local.common_policies, local.prod_accounts): "${pair[0]}-${pair[1]}" => {
+      policy_id = pair[0]
+      account_id = pair[1]
+    }
   }
 
 }
